2aaebb96a87cd89b21e4df11ba162b21a46ec057
SLCKDM <43304005+LevenusSupremus@users.noreply.github.com>
2021-06-29 15:18:16 +0300

    Update settings.py

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^gcr(hfc+ax_58pt3&mm)7q4^qiip*kmqi&&0z94u2(qldxy6k'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
